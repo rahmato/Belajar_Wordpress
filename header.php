65ce320160c26d361163c07bc72ab067bd1c0129
Mister Telo <mister@telo.com>
2019-10-17 16:14:44 +0700

    edit footer and edit header

--- header.php
+++ header.php
@@ -1,8 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head><title>Learning Wordpress</title></head>
+<body>
+
 <?php
 
 include('index.php');
 
-$duck = "coba main main";
+$duck = "coba main main saja";
 
 echo $duck;
 
